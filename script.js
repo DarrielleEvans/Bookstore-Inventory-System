473c9a322bd461fb1f4926da908b3ffe850d2a7a
DarrielleEvans <darriellehenderson@yahoo.com>
2022-04-12 19:01:34 -0500

    created navbar and menu button

---
+++ script.js
@@ -0,0 +1,11 @@
+//menu 
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+    document.getElementById("mySidepanel").style.height = "900px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0px";
+    document.getElementById("mySidepanel").style.height = "0px";
+  }
