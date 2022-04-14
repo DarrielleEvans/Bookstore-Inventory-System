13e609a5bb5fe77c47a9aa31bc6ef8d90cbbef8c
DarrielleEvans <darriellehenderson@yahoo.com>
2022-04-13 21:01:15 -0500

    added modal to add books

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//index.html script
 //menu 
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
@@ -66,7 +67,7 @@ localStorage.setItem("employeearray", storeEmployeeProfiles);
 
 const storeSiteProfiles = JSON.stringify(siteProfiles);
 localStorage.setItem("userArray", storeSiteProfiles);
-
+/*
 //check username and password to login to site
 let forms = document.getElementById("formLogin");
 console.log(forms);
@@ -89,13 +90,66 @@ function submitLogin(e) {
         e.preventDefault();
         window.location.href = "Inventory.html";
       }else{
-        alert("incorrect username or password");
+        noLogin();
         e.preventDefault();
       }
         break;
     }
 }
+//show incorrect message if login fails
+function noLogin(){
+    let loginContainer = document.getElementById("noMessage");
+    const incorrectMessage = document.createTextNode("incorrect username or password");
+    loginContainer.appendChild(incorrectMessage);
+}
+*/
 
+
+//inventory.html script
+    //class that defines properties for book information
+
+class Book {
+    constructor(title, author, price, isbn, genre, quanity) {
+      this.title = title;
+      this.author = author;
+      this.price=price;
+      this.isbn = isbn;
+      this.genre=genre;
+      this.quantity=quanity;
+      
+    }
+  }
+     //modal script
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var modalBtn = document.getElementById("modalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+modalBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+    //script to add book to the list
+
+    
 
 
 
